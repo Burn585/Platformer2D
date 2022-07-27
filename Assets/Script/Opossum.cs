d7e6a9a7cf6cd4e39fbf5d07dc38fd170c45c313
Burn585 <levshakov.pavel@yandex.ru>
2022-07-27 15:27:42 +0300

    Add opossum

---
+++ Assets/Script/Opossum.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opossum : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private float _speed = 1f;
+
+    private Transform[] _points;
+    private Transform target;
+    private Vector2 direction;
+    private int _currentPoint;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        target = _points[_currentPoint];
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+
+        if(transform.position == target.position)
+        {
+            _currentPoint++;
+
+            if(_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+        }
+
+        direction = (target.position - transform.position).normalized;
+
+        if(direction.x > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+        }
+    }
+}
